53af8fd0b52921f9655205b8db08a72697b5f9f8
edutak <edutak.ssafy@gmail.com>
2019-03-19 00:26:03 +0000

    03|Board model 생성|

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Board
 # Register your models here.
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'hit', 'created_at', 'updated_at']
+    
+admin.site.register(Board, BoardAdmin)
