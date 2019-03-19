4a029470d578e799cbce7bb2b9a899eedce8f915
edutak <edutak.ssafy@gmail.com>
2019-03-19 00:15:41 +0000

    02|settings.py|

--- django_fbv/settings.py
+++ django_fbv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')uv*(a8n^@&4%v6#cen*csgi@a*a6f%9vc!1#xom&sijofowkr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'boards',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
